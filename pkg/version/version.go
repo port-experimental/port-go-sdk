4620acad06f0be912be5a34ee5711f944de7480a
uday Korlimarla <222185270+udayk-port@users.noreply.github.com>
2025-12-06 03:19:51 +1100

    Release v0.2.2

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 import "strings"
 
 // Version follows semantic versioning (vMAJOR.MINOR.PATCH).
-const Version = "v0.2.1"
+const Version = "v0.2.2"
 
 // UserAgent returns the default user agent string shared by the SDK.
 func UserAgent() string {
